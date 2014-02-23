5ec216821b6c1865b3577749bf7342d5959e116b
jguerinet <julien@guerinet.com>
2014-02-23 12:20:39 -0500

    Test15 - After

--- src/ca/mcgill/amt/MainActivity.java
+++ src/ca/mcgill/amt/MainActivity.java
@@ -2,14 +2,24 @@ package ca.mcgill.amt;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    private EditText mInputA;
+    private TextView mResult;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInputA = (EditText)findViewById(R.id.inputA);
+        mResult = (TextView)findViewById(R.id.result);
+    }
+
+    public void calculate(View v){
+        mResult.setText("Error: Please input a value for side A");
     }
 }
